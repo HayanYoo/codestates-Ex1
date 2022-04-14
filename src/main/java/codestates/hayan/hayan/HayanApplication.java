48e436f3de93d592f466829dd342e55c44290fd7
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-14 13:56:08 +0900

    feat test function frame 생성

---
+++ src/main/java/codestates/hayan/hayan/HayanApplication.java
@@ -0,0 +1,13 @@
+package codestates.hayan.hayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HayanApplication.class, args);
+    }
+
+}
